3c2ad01dc2738f75fa00ceb26f08a3eb073d023d
JunarisAlf <junaris0715@gmail.com>
2022-10-20 13:55:33 +0700

    Add Sequelize Validation

--- models/photo.js
+++ models/photo.js
@@ -1,26 +1,52 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const {Model} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Photo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Photo extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  Photo.init({
-    title: DataTypes.STRING,
-    caption: DataTypes.TEXT,
-    poster_image_url: DataTypes.TEXT,
-    UserId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Photo',
-  });
-  return Photo;
+    Photo.init(
+        {
+            title: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty:{
+                        args: true,
+                        msg: "Title  tidak boleh kosong"
+                    }
+                }
+            },
+            caption: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty:{
+                        args: true,
+                        msg: "Caption tidak boleh kosong"
+                    }
+                }
+            },
+            poster_image_url: { 
+                type: DataTypes.TEXT,
+                validate: {
+                    notEmpty: {
+                        args: true,
+                        msg: "Profile Image URL tidak boleh kosong"
+                    },
+                    isUrl: true
+                }
+            },
+            UserId: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: 'Photo',
+        }
+    );
+    return Photo;
 };
